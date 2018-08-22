39818ec63174814797de7be04a42897f79ccce50
Christopher Arnold <chrisarnold2010@gmail.com>
2018-08-22 16:11:13 -0500

    added login and logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,23 @@ class User < ApplicationRecord
   has_secure_password
   has_many :comments
   has_many :pictures
+  # accepts_nested_attributes_for :pictures
+  # accepts_nested_attributes_for :comments
+
+  # def pictures_attributes=(picture_attributes)
+  #   picture_attributes.values.each do |picture_attribute|
+  #     picture = Picture.find_or_create_by(picture_attribute)
+  #     self.pictures << picture
+  #   end
+  # end
+
+  # def comments_attributes=(comment_attributes)
+  #   comment_attributes.values.each do |comment_attribute|
+  #     comment = Comment.find_or_create_by(comment_attribute)
+  #     self.comments << comment
+  #   end
+  # end
+
   validates :username, uniqueness: true
   validates :email, uniqueness: true
 
